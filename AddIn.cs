17c0d3a57acb9e3d66086f8605c4d775fb895878
Christopher Tso <christso@gmail.com>
2016-05-30 08:20:52 +1000

    initial commit

---
+++ AddIn.cs
@@ -0,0 +1,33 @@
+﻿using ExcelDna.Integration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADOMD = Microsoft.AnalysisServices.AdomdClient;
+
+namespace DG2NTT.DaxDrill
+{
+    public class AddIn : IExcelAddIn
+    {
+        public void AutoClose()
+        {
+
+        }
+
+        public void AutoOpen()
+        {
+            
+        }
+
+        [ExcelCommand(MenuName = "&DaxDrill", MenuText = "DrillThrough")]
+        public static void DrillThrough()
+        {
+            var commandText = "EVALUATE TOPN ( 10, Usage)";
+            var client = new DaxClient();
+            var cnn = new ADOMD.AdomdConnection("Provider=MSOLAP.5;Integrated Security=SSPI;Persist Security Info=True;Initial Catalog=Roaming;Data Source=localhost;");
+            var result = client.ExecuteQuery(commandText, cnn);
+            
+        }
+    }
+}
---
+++ DaxClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADOMD = Microsoft.AnalysisServices.AdomdClient;
+
+namespace DG2NTT.DaxDrill
+{
+    public class DaxClient
+    {
+        public DataTable ExecuteQuery(string qry, ADOMD.AdomdConnection cnx)
+        {
+            ADOMD.AdomdDataAdapter currentDataAdapter = new ADOMD.AdomdDataAdapter(qry, cnx);
+            DataTable tabularResults = new DataTable();
+            currentDataAdapter.Fill(tabularResults);
+            return tabularResults;
+        }
+    }
+}
